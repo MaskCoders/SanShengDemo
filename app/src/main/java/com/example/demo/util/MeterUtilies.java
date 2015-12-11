8fe461668bb126a23affcd119f1cb6f2f37f8377
sunshaogang <sunshaogang@kingsoft.com>
2015-12-11 16:29:21 +0800

    功能完成
    Signed-off-by: sunshaogang <sunshaogang@kingsoft.com>
    
    Conflicts:
    	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/demo/util/MeterUtilies.java
+++ app/src/main/java/com/example/demo/util/MeterUtilies.java
@@ -1,6 +1,11 @@
 package com.example.demo.util;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.text.TextUtils;
+import com.example.demo.mode.Meter;
 
 import java.text.SimpleDateFormat;
 
@@ -8,6 +13,9 @@ import java.text.SimpleDateFormat;
  * Created by sunshaogang on 12/10/15.
  */
 public class MeterUtilies {
+    public static final int NAME_MAX_BYTE_LENGTH = 30;
+
+    public static final String PARAM_METER = "meter";
     public static String getDateShort(Context context, long time) {
         FormattedDateBuilder dateBuilder = new FormattedDateBuilder(context);
         return dateBuilder.formatShortDate(time).toString();
@@ -21,5 +29,42 @@ public class MeterUtilies {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String dateString = formatter.format(time);
         return dateString;
+    }
+
+    public static int showFragment(FragmentManager manager, Fragment old, Fragment fragment, int id, int transition, String tag) {
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.setTransition(transition);
+        if (old != null && old.isVisible()) {
+            fragmentTransaction.hide(old);
+        }
+        if (fragment.isAdded()) {
+            fragmentTransaction.show(fragment);
+        }else {
+            fragmentTransaction.add(id, fragment, tag);
+        }
+        fragmentTransaction.addToBackStack(null);
+        final int transactionId = fragmentTransaction.commitAllowingStateLoss();
+        return transactionId;
+    }
+
+    public static boolean compareMeter(Meter a, Meter b){
+        if (a.mId != b.mId) {
+            return false;
+        } else if (a.mMeterID != b.mMeterID){
+            return false;
+        } else if (TextUtils.equals(a.mMeterName, b.mMeterName)){
+            return false;
+        } else if (a.mValueTime != b.mValueTime){
+            return false;
+        } else if (a.mReadTime != b.mReadTime){
+            return false;
+        } else if (a.mValz != b.mValz){
+            return false;
+        } else if (a.mDataType != b.mDataType){
+            return false;
+        } else if (a.isImportant != b.isImportant){
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/demo/view/BaseActivity.java
+++ app/src/main/java/com/example/demo/view/BaseActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import com.example.demo.R;
 
@@ -14,14 +14,21 @@ import com.example.demo.R;
  */
 public class BaseActivity extends Activity {
     private ActionBar mActionBar;
-    private RelativeLayout mActionBarView;
+    private FrameLayout mActionBarView;
     public final static int DEFAULT_VIEW = -1;//默认
     public final static int GENERAL_VIEW = 0;//主页
     public final static int SOCKET_VIEW = 1;//通信
-    public final static int DATABASE_VIEW = 2;//数据库
+    public final static int METER_LIST_VIEW = 2;//数据库
     public final static int INSERT_DATABASE_VIEW = 3;//插入数据库
     public final static int DETAIL_VIEW = 4;//详情
     public final static int MODIFY_DETAIL_VIEW = 5;//修改详情
+    private ActionBarCallback mActionBarCallback;
+
+    public interface ActionBarCallback{
+        void onSaveClick();
+        void onCancleClick();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +37,7 @@ public class BaseActivity extends Activity {
     private void customizeActionbar() {
         mActionBar = getActionBar();
         if (mActionBar != null) {
-            mActionBarView = (RelativeLayout) getLayoutInflater().inflate(
+            mActionBarView = (FrameLayout) getLayoutInflater().inflate(
                     R.layout.base_actionbar_view, null);
             mActionBarView.findViewById(R.id.ic_back).setOnClickListener(
                     new View.OnClickListener() {
@@ -45,10 +52,31 @@ public class BaseActivity extends Activity {
             mActionBar.setDisplayShowHomeEnabled(false);
             mActionBar.setDisplayShowTitleEnabled(false);
             setActionBar(DEFAULT_VIEW);
+            mActionBarView.findViewById(R.id.ab_modify_save).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mActionBarCallback != null) {
+                        mActionBarCallback.onSaveClick();
+                    }
+                }
+            });
+            mActionBarView.findViewById(R.id.ab_modify_cancle).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mActionBarCallback != null) {
+                        mActionBarCallback.onCancleClick();
+                    }
+                }
+            });
         }
     }
 
-    protected void setActionBar(final int mode) {
+    protected void setActionBar(final int mode){
+        setActionBar(mode, null);
+    }
+
+    protected void setActionBar(final int mode, ActionBarCallback callback) {
+        mActionBarCallback = callback;
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -59,8 +87,10 @@ public class BaseActivity extends Activity {
                     case SOCKET_VIEW:
                         ((TextView) mActionBarView.findViewById(R.id.ab_title)).setText("通讯");
                         break;
-                    case DATABASE_VIEW:
+                    case METER_LIST_VIEW:
                         ((TextView) mActionBarView.findViewById(R.id.ab_title)).setText("数据库展示");
+                        mActionBarView.findViewById(R.id.ab_modify_view).setVisibility(View.GONE);
+                        mActionBarView.findViewById(R.id.ab_view).setVisibility(View.VISIBLE);
                         break;
                     case INSERT_DATABASE_VIEW:
                         ((TextView) mActionBarView.findViewById(R.id.ab_title)).setText("生成测试数据");
@@ -69,13 +99,17 @@ public class BaseActivity extends Activity {
                         ((TextView) mActionBarView.findViewById(R.id.ab_title)).setText("电表数据详情");
                         break;
                     case MODIFY_DETAIL_VIEW:
-                        ((TextView) mActionBarView.findViewById(R.id.ab_title)).setText("修改电表数据");
+                        ((TextView) mActionBarView.findViewById(R.id.ab_modify_title)).setText("修改电表数据");
+                        mActionBarView.findViewById(R.id.ab_modify_view).setVisibility(View.VISIBLE);
+                        mActionBarView.findViewById(R.id.ab_view).setVisibility(View.GONE);
                         break;
 
                     default:
                         ((TextView) mActionBarView.findViewById(R.id.ab_title)).setText(R.string.company_name);
+                        break;
                 }
             }
         });
     }
+
 }
