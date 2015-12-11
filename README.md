40235b22d4d800b86fc4155ec10f76f95b5de836
zhangbaohua <zhangbaohua@kingsoft.com>
2015-12-11 15:29:03 +0800

    大体实现了socket demo

--- README.md
+++ README.md
@@ -3,3 +3,29 @@
 
 开发在自己分支，然后合并到master
 
+目前我们筛选出来两家，为了进一步确定谁合适。
+我们出一个简单的需求，要求3天之内，做一个demo出来，自由发挥。我们支付5000元。
+然后我们再比较一下，看谁做的好。来决定和谁合作。
+
+
+按目前了解的需求制作一个完整的可运行的app程序
+
+1. 首页，要包含功能导航
+2. sqlite数据库表操作展示，制作内容展示，可无限下拉和编辑页（可编辑每条记录的内>容），表结构如下
+
+[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL         标识
+,[MeterId] INTEGER NOT NULL      电表标识
+,[MeterName] CHAR(20) NOT NULL   电表名称  显示为文本
+,[ValueTime] DATETIME NOT NULL   数据时标  按长时间格式显示 如2001-1-1 01:01:01
+,[ReadTime] DATETIME NOT NULL    读取时间  同上
+,[DtId] INTEGER NOT NULL         数据类型  显示值对应的文本 *注1
+,[Valz] FLOAT NOT NULL
+,[IsImportant] BOOLEAN           是否重要
+
+注1：数据类型DtId的值 1显示为“日冻结”  2显示为“实时数据”
+
+3. 实现一个基于socket（Tcp）的服务，侦听8001端口，可以通过网络连接，连接后可以收
+发二进制数据，收发的报文可以显示在界面上
+比如：客户端发送 2个字节报文 0x68 0x16 服务端返回0x16 0x68
+
+
--- app/src/main/java/com/example/demo/view/SocketDemo.java
+++ app/src/main/java/com/example/demo/view/SocketDemo.java
@@ -1,117 +1,152 @@
 package com.example.demo.view;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.example.demo.R;
 
-public class SocketDemo extends Activity implements Runnable {
+import java.io.*;
+import java.net.Socket;
+
+public class SocketDemo extends Activity implements View.OnClickListener {
     private TextView tv_msg = null;
+    private TextView tv_info = null;
     private EditText ed_msg = null;
     private Button btn_send = null;
+    private Button btn_conn = null;
+    private Button btn_cls = null;
     // private Button btn_login = null;
-    private static final String HOST = "10.0.2.2";
-    private static final int PORT = 9999;
+    private static final String HOST = "192.168.134.82";
+    private static final int PORT = 8001;
     private Socket socket = null;
     private BufferedReader in = null;
     private PrintWriter out = null;
-    private String content = "";
+
+    private static final int CONN_SER_CLS = -2;
+    private static final int CONN_ERR = -1;
+    private static final int CONN_OK = 0;
+    private static final int RECV_MSG = 1;
+    private static final int INPUT_ERR = -3;
+    private static final String ERRCODE = "ERRCODE";
+    private static final String CONN_SUCCESS = "CONN_SUCCESS";
+
     //接收线程发送过来信息，并用TextView显示
     public Handler mHandler = new Handler() {
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            tv_msg.setText(content);
+            String content = msg.obj.toString();
+            switch(msg.what){
+                case CONN_SER_CLS:
+                    break;
+                case RECV_MSG:
+                    if(content.contains(ERRCODE)){
+                        content = "Service can not resolve your code ";
+                    }else if (content.contains(CONN_SUCCESS)){
+                        tv_info.setText("Ser:"+content.replace(CONN_SUCCESS,"").replace("\n","")+",Port:"+PORT);
+                        return;
+                    }
+                    break;
+                case CONN_OK:
+                    break;
+                case CONN_ERR:
+                    content = "connect err: "+content;
+                    default:
+            }
+            tv_msg.setText(tv_msg.getText().toString()+"\n SER => "+content);
         }
     };
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
-
-        tv_msg = (TextView) findViewById(R.id.tv);
+        tv_msg = (TextView) findViewById(R.id.log);
+        tv_info = (TextView) findViewById(R.id.info);
+        tv_msg.setMovementMethod(ScrollingMovementMethod.getInstance());
         ed_msg = (EditText) findViewById(R.id.et);
-        btn_send = (Button) findViewById(R.id.btn);
+        btn_send = (Button) findViewById(R.id.send);
+        btn_conn = (Button) findViewById(R.id.conn);
+        btn_cls = (Button) findViewById(R.id.cls);
+        ed_msg.setText("0x68 0x16");
+        btn_send.setOnClickListener(this);
+        btn_conn.setOnClickListener(this);
+        btn_cls.setOnClickListener(this);
+        //启动线程，接收服务器发送过来的数据
+    }
 
-        try {
-            socket = new Socket(HOST, PORT);
-            in = new BufferedReader(new InputStreamReader(socket
-                    .getInputStream()));
-            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-                    socket.getOutputStream())), true);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            ShowDialog("login exception" + ex.getMessage());
-        }
-        btn_send.setOnClickListener(new Button.OnClickListener() {
+    private Message getMessage(String content,int type){
+        Message msg = new Message();
+        msg.obj = content;
+        msg.what = type;
+        return msg;
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.conn:
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            if(socket == null ||socket.isClosed()) {
+                                socket = new Socket(HOST, PORT);
+                                in = new BufferedReader(new InputStreamReader(socket
+                                        .getInputStream()));
+                                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                                        socket.getOutputStream())), true);
+                            }
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
+                            while (true) {
+                                if (!socket.isClosed()) {
+                                    if (socket.isConnected()) {
+                                        if (!socket.isInputShutdown()) {
+                                            String  content = null;
+                                            if ((content = in.readLine()) != null) {
+                                                content += "\n";
+                                                mHandler.sendMessage(getMessage(content,RECV_MSG));
+                                            } else {
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                            Log.d("", "login exception" + ex.getMessage());
+                            mHandler.sendMessage(getMessage("conn err",CONN_ERR));
+                        }
+                    }
+                });
+                thread.start();
+                break;
+
+            case R.id.send:
                 String msg = ed_msg.getText().toString();
-                if (socket.isConnected()) {
+                if(TextUtils.isEmpty(msg)){
+                    Toast.makeText(this,"INPUT IS NOT NULL ",0).show();
+                    break;
+                }
+                tv_msg.setText(tv_msg.getText().toString()+"\n CLIENT => "+msg);
+                if (socket != null && socket.isConnected()) {
                     if (!socket.isOutputShutdown()) {
                         out.println(msg);
                     }
                 }
-            }
-        });
-        //启动线程，接收服务器发送过来的数据
-        new Thread(SocketDemo.this).start();
-    }
-
-    /**
-     * 如果连接出现异常，弹出AlertDialog！
-     */
-    public void ShowDialog(String msg) {
-        new AlertDialog.Builder(this).setTitle("notification").setMessage(msg)
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                }).show();
-    }
-
-    /**
-     * 读取服务器发来的信息，并通过Handler发给UI线程
-     */
-    public void run() {
-        try {
-            while (true) {
-                if (!socket.isClosed()) {
-                    if (socket.isConnected()) {
-                        if (!socket.isInputShutdown()) {
-                            if ((content = in.readLine()) != null) {
-                                content += "\n";
-                                mHandler.sendMessage(mHandler.obtainMessage());
-                            } else {
-
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+                break;
+            case R.id.cls:
+                tv_msg.setText("");
+                break;
         }
     }
-
 }
