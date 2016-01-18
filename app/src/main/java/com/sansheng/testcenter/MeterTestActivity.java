a83665f82258d3edb2e411c265cddf0d8cd12595
Sun <lostghoul@gmail.com>
2016-01-18 22:31:05 +0800

    修改默认json
    Signed-off-by: Sun <lostghoul@gmail.com>

--- app/src/main/java/com/sansheng/testcenter/MeterTestActivity.java
+++ app/src/main/java/com/sansheng/testcenter/MeterTestActivity.java
@@ -169,7 +169,7 @@ public class MeterTestActivity extends BaseActivity implements IServiceHandlerCa
         String result = EquipmentPreference.getPreferences(this).getSelectedMeterTest();
         Log.e("ssg", "result = " + result);
         if (TextUtils.isEmpty(result)) {
-            result = "[\"0\",\"1\",\"2\"][\"0\",\"1\",\"2\"]";
+            result = "[\"0\",\"1\",\"2\"]";
         }
         mAdapter.setSelectedItemts(ModuleUtilites.jsonToMapForMeterTest(result, getResources().getStringArray(R.array.meter_test_items)));
         mListView.setAdapter(mAdapter);
