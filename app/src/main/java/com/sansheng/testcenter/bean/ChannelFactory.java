3309fd9577e617f7105904d0607e44805608d9d2
zhangbaohua <zhangbaohua@kingsoft.com>
2016-01-26 10:31:51 +0800

    修改了个种通讯的频率

--- app/src/main/java/com/sansheng/testcenter/bean/ChannelFactory.java
+++ app/src/main/java/com/sansheng/testcenter/bean/ChannelFactory.java
@@ -10,6 +10,11 @@ import com.sansheng.testcenter.tools.serial.SerialHelper;
 public class ChannelFactory {
     public static SerialHelper INFRA_RED;
     public static String INFRA_RED_DN = "/dev/xxx";
+    public static String RS485_1 = "/dev/xxx";
+    public static String RS485_2 = "/dev/xxx";
+    public static String RS232 = "/dev/xxx";
+    public static String ZB = "/dev/xxx";
+    public static String ZB2 = "/dev/xxx";
 
     public static SerialHelper getInstance(int index,boolean isMeter, MainHandler handler, IServiceHandlerCallback cb){
         SerialHelper tmp = null;
@@ -19,22 +24,22 @@ public class ChannelFactory {
                     tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
                     break;
                 case 1:
-                    tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
+                    tmp = new SerialHelper(RS485_1,1200,handler,cb);
                     break;
                 case 2:
-                    tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
+                    tmp = new SerialHelper(RS485_2,2400,handler,cb);
                     break;
                 case 3:
-                    tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
+                    tmp = new SerialHelper(RS232,9600,handler,cb);
                     break;
                 case 4:
-                    tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
+                    tmp = new SerialHelper(ZB2,115200,handler,cb);
                     break;
             }
         }else{
             switch (index) {
                 case 3:
-                    tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
+                    tmp = new SerialHelper(RS485_1,1200,handler,cb);
                     break;
                 case 4:
                     tmp = new SerialHelper(INFRA_RED_DN,1200,handler,cb);
