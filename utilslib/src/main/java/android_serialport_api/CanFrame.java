57381c0772c6332c130102e410ad3e2c1fa312d9
Sun <lostghoul@gmail.com>
2016-03-15 10:14:52 +0800

    delete useless modules
    Signed-off-by: Sun <lostghoul@gmail.com>

--- utilslib/src/main/java/android_serialport_api/CanFrame.java
+++
@@ -1,7 +0,0 @@
-package android_serialport_api;
-
-public class CanFrame {
-	public  int can_id;
-	public   char can_dlc;
-	public String data;
-}
--- utilslib/src/main/java/com/sansheng/testcenter/tools/protocol/TerUserDataParseInterface.java
+++
@@ -1,8 +0,0 @@
-package com.sansheng.testcenter.tools.protocol;
-
-/**
- * Created by hua on 16-2-18.
- */
-public interface TerUserDataParseInterface {
-    public Object parse();
-}
